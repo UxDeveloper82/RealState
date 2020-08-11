fa7f519c4ffbd5b90cfb6091d7613bd08817e0de
UxDeveloper82 <electronicsmf@aol.com>
2020-08-11 07:40:45 -0400

    adding links in navbar

--- Frontend/src/app/routes.ts
+++ Frontend/src/app/routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent }
-
+  { path: 'home', component: HomeComponent },
+  { path: 'properties', component: PropertyListComponent}
 ];
