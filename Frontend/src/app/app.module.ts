ea26964f1dceb746ab1d76305b79466bff1951b3
UxDeveloper82 <electronicsmf@aol.com>
2020-08-10 21:04:33 -0400

    adding boostrap 4

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyListComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule
