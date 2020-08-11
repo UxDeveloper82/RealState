638b0ce4b9ff0a0d43906306b61dbeeea9d9cba6
UxDeveloper82 <electronicsmf@aol.com>
2020-08-11 07:33:28 -0400

    Adding router for navigation

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 import { AppComponent } from './app.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyListComponent,
     PropertyCardComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
