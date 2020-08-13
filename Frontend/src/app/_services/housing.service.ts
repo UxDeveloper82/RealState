4c9e0347f34b8ac53945ab32ed0b43a89c5a944c
UxDeveloper82 <electronicsmf@aol.com>
2020-08-13 07:46:25 -0400

    adding more in service andsore and searching  list page

--- Frontend/src/app/_services/housing.service.ts
+++ Frontend/src/app/_services/housing.service.ts
@@ -11,28 +11,68 @@ export class HousingService {
 
 constructor(private http: HttpClient) { }
 
-  // tslint:disable-next-line: typedef
-  getProperty(id: number) {
-    return this.getAllProperties().pipe(
-      map(propertiesArray => {
-        // throw new Error('Some error');
-        return propertiesArray.find(p => p.Id === id);
-      })
-    );
+getProperty(id: number) {
+  return this.getAllProperties().pipe(
+    map(propertiesArray => {
+      // throw new Error('Some error');
+      return propertiesArray.find(p => p.Id === id);
+    })
+  );
+}
+
+getAllProperties(SellRent?: number): Observable<Property[]> {
+  return this.http.get('data/properties.json').pipe(
+    map(data => {
+    const propertiesArray: Array<Property> = [];
+    const localProperties = JSON.parse(localStorage.getItem('newProp'));
+
+    if (localProperties) {
+      for (const id in localProperties) {
+        if (SellRent) {
+        if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent) {
+          propertiesArray.push(localProperties[id]);
+        }
+      } else {
+        propertiesArray.push(localProperties[id]);
+      }
+      }
+    }
+
+    for (const id in data) {
+      if (SellRent) {
+        if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
+          propertiesArray.push(data[id]);
+        }
+        } else {
+          propertiesArray.push(data[id]);
+      }
+    }
+    return propertiesArray;
+    })
+  );
+
+  return this.http.get<Property[]>('data/properties.json');
+}
+addProperty(property: Property) {
+  let newProp = [property];
+
+  // Add new property in array if newProp alreay exists in local storage
+  if (localStorage.getItem('newProp')) {
+    newProp = [property,
+                ...JSON.parse(localStorage.getItem('newProp'))];
   }
-   // tslint:disable-next-line: typedef
-   getAllProperties(): Observable<Property[]>{
-     return this.http.get('data/properties.json').pipe(
-       map( data => {
-           const propertiesArray: Array<Property> = [];
-           for( const id in data) {
-             if (data.hasOwnProperty(id)) {
-              propertiesArray.push(data[id]);
-             }
-           }
-           return propertiesArray;
-       })
-     );
-   }
+
+  localStorage.setItem('newProp', JSON.stringify(newProp));
+}
+
+newPropID() {
+  if (localStorage.getItem('PID')) {
+    localStorage.setItem('PID', String(+localStorage.getItem('PID') + 1));
+    return +localStorage.getItem('PID');
+  } else {
+    localStorage.setItem('PID', '101');
+    return 101;
+  }
+}
 
 }
